1a85fe19180f6eb4b16df1ad52318e12879397c2
89081314528 <89081314528@mail.ru>
2019-10-19 21:33:02 +0300

    19/10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -8,7 +9,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
         for (int i = 1; i < 10; i++) {
-            int karta = (int) (Math.random() * (12 - 2)) + 2;
+            int karta = (int) (Math.random() * (11 - 2)) + 2;
             System.out.println("Ваша " + i + " карта " + karta + " очков");
             sum = sum + karta;
             System.out.println("Сумма ваших карт " + sum);
@@ -16,27 +17,30 @@ public class Main {
                 System.out.println("Вы проиграли");
                 return;
             }
-            System.out.println("Если будете брать еще карту, введите да");
+            System.out.println("Если будете брать еще карту, введите да, если нет, введите нет");
             String a = scanner.next();
             if (a.equals("да")) {
             } else {
                 break;
             }
         }
-        int kartaKrupie = (int) (Math.random() * (26 - 16)) + 16;
+//        int kartaKrupie = (int) (Math.random() * (25 - 16)) + 16;
+        //работает быстрее
+        int kartaKrupie = (int) (ThreadLocalRandom.current().nextInt(16,25));
         System.out.println("У крупье " + kartaKrupie + " очков");
         if (kartaKrupie > 21) {
-            System.out.println("У крупье больше 21, вы выиграли");
+            System.out.println("У крупье больше 21, перебор, вы выиграли");
             return;
         }
-
         if (sum > kartaKrupie) {
-            System.out.println("Вы выиграли!!!!!!!!!");
+            System.out.println("У вас больше очков, чем у крупье, Вы выиграли!!!!!!!!!");
+            return;
         }
         if (sum < kartaKrupie) {
-            System.out.println("Вы проиграли");
+            System.out.println("У вас меньше очков, чем у крупте, Вы проиграли");
+            return;
         } else {
-            System.out.println("Ничья, попробуйте еще раз");
+            System.out.println("У вас одинаковое количество очков с крупье, Ничья, попробуйте еще раз");
         }
     }
 }
